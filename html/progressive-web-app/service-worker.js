cd73866dc4a226aa1dcca7bb5c090addf800903b
paranerd <paranerd.development@gmail.com>
2019-12-02 19:15:14 +0100

    PWA fixed offline issue

--- html/progressive-web-app/service-worker.js
+++ html/progressive-web-app/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 
 const FILES_TO_CACHE = [
 	'/',
@@ -8,24 +8,42 @@ const FILES_TO_CACHE = [
 	'/assets/icon.png'
 ];
 
-self.addEventListener('install', function() {
+self.addEventListener('install', function(event) {
 	console.log('[ServiceWorker] Install');
 
 	self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-	console.log('[ServiceWorker] Activate');
-
-	self.clients.claim();
-});
-self.addEventListener('fetch', function(event) {
-	console.log('Fetch!', event.request);
 
 	event.waitUntil(
 		caches.open(CACHE_NAME).then((cache) => {
 			console.log('[ServiceWorker] Pre-caching offline page');
 			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
+});
+
+self.addEventListener("activate", event => {
+	console.log('[ServiceWorker] Activate');
+
+	event.waitUntil(
+		caches.keys().then(function(keyList) {
+			return Promise.all(keyList.map(function(key) {
+				if (key !== CACHE_NAME) {
+					console.log('[ServiceWorker] Removing old cache', key);
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
+
+	self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log('Fetch!', event.request);
+
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
 		})
 	);
 });
